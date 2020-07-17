39b688a77acb629168fd9ec2a67f0d0e062a8e7c
Darren Vasquez <dvasque4@gmail.com>
2020-07-16 21:31:27 -0500

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -15,20 +15,22 @@ async def test(ctx, arg):
     pass
 
 @cmdtest.command()
-async def count(ctx, user: discord.User):
+async def count(ctx, user: discord.User=None):
+    today = 0
+    total = 0
     if user:
-        for row in cr.execute(''' SELECT * FROM users where id='{0}' '''.format(user.id)):
-            #print(row)
-            await ctx.channel.send('{0} has sent {1} messages while this bot was online.'.format(user.mention, row[1]))
-            return
-        await ctx.channel.send('{0} has sent 0 messages while this bot was online.'.format(user.mention))
+        for row in cr.execute(''' SELECT * FROM users where id='{0}' and day=date('now') '''.format(user.id)):
+            today = row[3]
+        for row in cr.execute(''' SELECT SUM(msgCount) FROM users where id='{0}' '''.format(user.id)):
+            total = row[0]
+        await ctx.channel.send('{0} has sent {1} messages today, and {2} total messages while this bot was online.'.format(user.mention, today, total))
     else:
-        for row in cr.execute(''' SELECT * FROM users where id='{0}' '''.format(ctx.author.id)):
-            #print(row)
-            await ctx.channel.send('{0} has sent {1} messages while this bot was online.'.format(ctx.author.mention, row[1]))
-            return
-        await ctx.channel.send('{0} has sent 0 messages while this bot was online.'.format(ctx.author.mention))
-        return
+        for row in cr.execute(''' SELECT * FROM users where id='{0}' and day=date('now') '''.format(ctx.author.id)):
+            today = row[3]
+        for row in cr.execute(''' SELECT SUM(msgCount) FROM users where id='{0}' '''.format(ctx.author.id)):
+            total = row[0]
+        await ctx.channel.send('{0} has sent {1} messages today, and {2} total messages while this bot was online.'.format(ctx.author.mention, today, total))
+    return
 
 @cmdtest.command()
 async def exit(ctx):
@@ -43,30 +45,28 @@ async def exit(ctx):
 @cmdtest.event
 async def on_ready():
     print('[SYSTEM] Logged in as {0.user}'.format(cmdtest))
-    await cmdtest.change_presence(activity=discord.Activity(name='chat games', type=discord.ActivityType.playing))
+    await cmdtest.change_presence(activity=discord.Activity(name='with ur mom', type=discord.ActivityType.playing))
 
 @cmdtest.event
 async def on_message(message):
     if message.author == cmdtest.user:
         return
-    elif ':YEP:' in message.content:
+    if (not message.content.startswith(";")) and (not "bot" in message.channel.name):
+        cr.execute(''' SELECT * FROM users where id='{0}' and day=date('now') '''.format(message.author.id))
+        if cr.fetchone():
+            cr.execute(''' UPDATE users SET msgCount = msgCount + 1 WHERE id='{0}' and day=date('now') '''.format(message.author.id))
+            db.commit()
+            #print("msgCount incremented for {0}".format(message.author.id))
+        else:
+            cr.execute(''' INSERT INTO users VALUES('{0}', '{1}', date('now'), 1)'''.format(message.author.id, message.author.name))
+            db.commit()
+            print("inserted row for user {0} ({1})".format(message.author.name, message.author.id))
+    if ':YEP:' in message.content:
         emoji = discord.utils.get(message.guild.emojis, name='YEP')
         #print('found')
         if emoji:
             await message.add_reaction(emoji)
         return
-    else:
-        cr.execute(''' SELECT * FROM users where id='{0}' '''.format(message.author.id))
-        if cr.fetchone():
-            cr.execute(''' UPDATE users SET msgCount = msgCount + 1 WHERE id='{0}' '''.format(message.author.id))
-            db.commit()
-            #print("msgCount incremented for {0}".format(message.author.id))
-        else:
-            cr.execute(''' INSERT INTO users VALUES('{0}', 1)'''.format(message.author.id))
-            db.commit()
-            print("inserted row for user {0}".format(message.author.id))
-    if 'bot' in message.channel.name:
-        print("bot channel!")
     await cmdtest.process_commands(message)
 
 ### Setup
@@ -75,5 +75,7 @@ db = sqlite3.connect('test.db')
 cr = db.cursor()
 cr.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='users' ''')
 if cr.fetchone()[0] != 1:
-    cr.execute(''' CREATE TABLE users (id text, msgCount integer) ''')
+    cr.execute(''' CREATE TABLE users (id text, tag text, day date, msgCount integer) ''')
     db.commit()
+
+cmdtest.run('<key here>')
