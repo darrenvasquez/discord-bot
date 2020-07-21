3abfaa62a68a7fbe2e35bfb5ef47cf555743a64c
Darren Vasquez <dvasque4@gmail.com>
2020-07-21 01:05:08 -0500

    Update reboot.sh

--- scripts/reboot.sh
+++ scripts/reboot.sh
@@ -1,11 +1,15 @@
 #/bin/bash
 
-screen -dmS discord-bot bash -c "
+for session in $(screen -ls | grep -o '[0-9]*\.discord-bot'); do screen -S "${session}" -X quit; done
+
+screen -dmS discord-bot
+
+screen -S discord-bot -X stuff "
 cd ~/discord-bot/
 
 echo '[Script] Pulling changes from Git...'
 git pull
 echo '[Script] Starting Discord Bot...'
-python3 discord-bot.py
+python3 discord_bot.py
 
 "
