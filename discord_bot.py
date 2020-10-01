d798ca6e08b23cf10eebbee6b717ff4cb3051579
Darren Vasquez <42012825+darrenvasquez@users.noreply.github.com>
2020-10-01 14:31:04 -0700

    Update discord_bot.py

--- discord_bot.py
+++ discord_bot.py
@@ -104,7 +104,7 @@ async def on_reaction_add(reaction, user):
                                     #embedMsg = discord.Embed(title="{0}#{1}".format(reaction.message.author.name, reaction.message.author.discriminator), timestamp=reaction.message.created_at, description=reaction.message.content, color=0x32a895)
                                     #embedMsg.set_thumbnail(url=reaction.message.author.avatar_url)
                                     #embedMsg.set_author(name="temp", url=discord.Embed.Empty, icon_url=reaction.message.author.avatar_url)
-                                    embed=discord.Embed(description=reaction.message.content)
+                                    embed=discord.Embed()
                                     embed.set_author(name="{0}#{1}".format(reaction.message.author.name, reaction.message.author.discriminator), url=reaction.message.jump_url, icon_url=reaction.message.author.avatar_url)
                                     embed.add_field(value=reaction.message.content)
                                     embed.set_footer(text="click username to jump to message")
